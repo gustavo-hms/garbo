dee37e6356170aeac23d6533d503bad52c15c22c
Gustavo <github@gustavo.claroenigma.com.br>
2020-11-03 21:56:37 -0300

    Missing fish files

---
+++ colors/garbo.fish
@@ -0,0 +1,26 @@
+set -L
+set -U fish_color_normal normal
+set -U fish_color_command 7a80ee 
+set -U fish_color_quote ff1ea1 
+set -U fish_color_redirection 00afff
+set -U fish_color_end 009900
+set -U fish_color_error ff0000
+set -U fish_color_param ccd0da 
+set -U fish_color_selection 000000 
+set -U fish_color_search_match 00e2ff --bold
+set -U fish_color_history_current --bold
+set -U fish_color_operator 838999 
+set -U fish_color_escape 838999 
+set -U fish_color_cwd 00b982 
+set -U fish_color_cwd_root ff0000 
+set -U fish_color_valid_path --underline
+set -U fish_color_autosuggestion 373940 
+set -U fish_color_user fffb79 
+set -U fish_color_host d3005b --bold
+set -U fish_color_cancel -r
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description fffb79 
+set -U fish_pager_color_prefix white --bold --underline
+set -U fish_pager_color_progress brwhite --background=cyan
+set -U fish_color_comment 595d68 
+set -U fish_color_match  --bold --underline
